eabf67ecc06c7e72d2613150df0e612a0cf252f9
SaquibFurniturewala1 <saquibfurniturewala1@gmail.com>
2025-03-06 14:45:41 -0500

    Implemented Completed Feature & Updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # To-Do App  
 
-A simple and efficient **To-Do App** built with HTML, CSS, and JavaScript.  
+A simple and efficient **To-Do App** built with HTML, CSS, and JavaScript, using local storage to save your tasks even after refreshing the webpage or closing it.
+
+## Screenshot
+
+![Screenshot 1](screenshot/img1.png)  
+![Screenshot 2](screenshot/img2.png)
 
 ## Features  
 - Add new tasks  
@@ -15,14 +20,14 @@ A simple and efficient **To-Do App** built with HTML, CSS, and JavaScript.
 
 ## Usage    
 - Type a task in the input field and click Add.
-- Click on a task to mark it as completed. (upcomming)
+- Click on a task to mark it as completed.
 - Click the delete button to remove a task.
 
 ## Author
-- 👤 Saquib Furniturewala
-- GitHub: @SaquibFurniturewala1
-- Frontend Mentor: @SaquibFurniturewala1
-- LinkedIn: Saquib Furniturewala
+ 👤 Saquib Furniturewala
+- GitHub: [@SaquibFurniturewala1](https://github.com/SaquibFurniturewala1)
+- Frontend Mentor:[ @SaquibFurniturewala1](https://www.frontendmentor.io/profile/SaquibFurniturewala1)
+- LinkedIn: [Saquib Furniturewala](https://www.linkedin.com/in/saquib-furniturewala-a873411a1/)
 
 ## Live DOMO
 
--- app.js
+++ app.js
@@ -3,22 +3,32 @@ const taskList = document.querySelector('#task-list');
 const addTask = document.querySelector('#add-task');
 const errorMsg = document.querySelector('#error-msg');
 
-loadTask () 
+loadTask()
 
 addTask.addEventListener('click', () => {
     if (taskInput.value.trim() === '') {
         errorMsg.textContent = 'Enter a Task'
         return;
-    }
+    } 
+    if (taskInput.value.length > 25) {
+        errorMsg.textContent = 'Task length should be less than 25 characters';
+        return;
+    }    
     renderList(taskInput.value);
-    saveTasks ()
+    saveTasks()
     taskInput.value = '';
     errorMsg.textContent = ''
 });
 
+
 function renderList(taskText) {
+
     let newList = document.createElement('li');
     newList.textContent = taskText;
+    newList.classList.add('new-list');
+    newList.style.cursor = 'Pointer'
+
+    markTaskAsCompleted()
 
     let deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Delete Task';
@@ -32,13 +42,34 @@ function renderList(taskText) {
 
     taskList.appendChild(taskItem);
 
+    function markTaskAsCompleted() {
+
+        newList.addEventListener('click', () => {
+            newList.classList.toggle('completed')
+            if (newList.classList.contains('completed')) {
+                deleteBtn.textContent = 'Task Completed'
+                deleteBtn.classList.add('completed-btn')
+                deleteBtn.classList.remove('delete-btn');
+                deleteBtn.disabled = true
+            } else {
+                deleteBtn.textContent = 'Delete Task'
+                deleteBtn.classList.add('delete-btn');
+                deleteBtn.classList.remove('completed-btn')
+                deleteBtn.disabled = false
+            }
+            saveTasks()
+        });
+    }
+
     deleteBtn.addEventListener('click', () => {
         taskItem.remove();
-        saveTasks ()
+        saveTasks();
     });
+
 };
 
-function saveTasks () {
+
+function saveTasks() {
     let tasks = []
 
     taskList.querySelectorAll('li').forEach(function (item) {
@@ -46,10 +77,11 @@ function saveTasks () {
     })
 
     localStorage.setItem('myList', JSON.stringify(tasks))
-}
+};
 
-function loadTask () {
-    const tasks =  JSON.parse(localStorage.getItem('myList')) || []
+
+function loadTask() {
+    const tasks = JSON.parse(localStorage.getItem('myList')) || []
 
     tasks.forEach(task => {
         renderList(task)
